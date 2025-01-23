799b5d46f7dea1699e1f7d24505f05fb2232fffd
Ndarama <ndaramartwork@gmail.com>
2025-01-23 12:23:22 +0200

    contact v1

--- contacts-api/importData.js
+++ contacts-api/importData.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Contact = require('./models/Contact');
+const Contact = require('./models/contact');
 const data = require('./data.json');
 
 dotenv.config();
